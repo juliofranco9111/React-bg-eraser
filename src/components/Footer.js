38c47951a0ece757a19ada3c9c6474589b061f63
Julio Franco <julio.franco9111@gmail.com>
2021-10-11 23:11:49 -0300

    done with registration of SW

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     <footer className="footer">      
-      <h3>Made with 💖 by <a target="_blank" rel="noreferrer" href="https://juliofranco.vercel.app/">Julio Franco</a></h3>
+      <h3>Made with 💖 by <a target="_blank" rel="noreferrer" href="https://juliofranco.vercel.app/">Julio Franco.</a></h3>
     </footer>
   );
 };
